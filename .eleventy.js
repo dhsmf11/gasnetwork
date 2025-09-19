1a855592e7a49d149dff012f516a3c72cdcfc017
dhsmf11 <dhsmfdhseh12@naver.com>
2025-09-19 10:19:42 +0900

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+	return {
+		dir: {
+			input: "src/pages",
+			includes: "../_Shared",
+			output: "dist",
+			data: "../_data",
+		},
+		templateFormats: ["njk", "md", "html"],
+		htmlTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
+	};
+};
+/*
+npm run build       # 정적 HTML 생성
+npm run serve       # 로컬 서버 실행
+*/
